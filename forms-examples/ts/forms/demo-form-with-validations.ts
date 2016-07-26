6bdc15376731dc6232e81a76cc2668d1b28743a4
Arun Chalise <Arun.Chalise@macquarie.com>
2016-07-26 16:19:26 +1000

    Two way binding in the input

--- forms-examples/ts/forms/demo-form-with-validations.ts
+++ forms-examples/ts/forms/demo-form-with-validations.ts
@@ -7,12 +7,17 @@ import { FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl
 	template : `		
 	    <div class='ui raised segment'>
 			<h2 class='ui header'>Demo form with validations</h2>
+			<div class='ui message'>Product name to go here {{productName}}</div>							
 			<form class='ui form' [ngFormModel]='myForm' (ngSubmit)='onSubmit(myForm.value)'>
 				<div class='field' [class.error]="!sku.valid && sku.touched">
 				   <label for='skuInput'>SKU</label>
 				   <input type='text' id='skuInput' [ngFormControl]="myForm.controls['sku']">
 				</div>
 				<div *ngIf='!sku.valid' class='ui error message'>SKU is invalid</div>
+				<div class='field' [class.error]="!productName.valid && productName.touched">
+				   <label for='productName'>Product</label>
+				   <input type='text' id='productName' [ngFormControl]="myForm.controls['productName']" [(ngModel)]="productName">
+				</div>				
 			    <div *ngIf='!myForm.valid' class='ui error message'>Form is invalid</div>				
                 <button type="submit" class="ui button">Submit</button>				
 			</form>
@@ -21,15 +26,20 @@ import { FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl
 export class DemoFormWithValidations {
 	myForm : ControlGroup;
 	sku : AbstractControl;
+	productName : AbstractControl;
+	
 
 	constructor(fb: FormBuilder) {
 		this.myForm = fb.group({
-			'sku' : ['', Validators.required]
+			'sku' : ['', Validators.required],
+			'productName' : ['', Validators.required]
 		});
 		this.sku = this.myForm.controls['sku'];
+		this.productName = this.myForm.controls['productName'];
 	}
 
 	onSubmit(value: String): void {
 	   console.log('you submitted value: ', value);
+	   console.log(this.sku.valueChanges);
 	}	
 }
